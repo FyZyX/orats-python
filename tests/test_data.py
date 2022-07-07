e42f84db9908785f225bb89f753310cc98637df9
Lucas Lofaro <lucasmlofaro@gmail.com>
2022-07-07 14:10:09 -0700

    add publish action

--- tests/test_data.py
+++ tests/test_data.py
@@ -9,20 +9,19 @@ from orats.model.underlying import Ticker
 
 
 def test():
-    path = os.path.join(os.path.dirname(__file__), 'fixtures', 'strike.json')
+    path = os.path.join(os.path.dirname(__file__), "fixtures", "strike.json")
     with open(path) as handle:
         strike = Strike(**json.load(handle))
     print(strike)
 
 
 class MyDataApi(unittest.TestCase):
-
     @classmethod
     def setUpClass(cls) -> None:
-        cls._api = DataApi('demo')
+        cls._api = DataApi("demo")
 
     def test_tickers(self):
-        tickers = self._api.tickers('IBM')
+        tickers = self._api.tickers("IBM")
         self.assertEqual(len(tickers), 1)
         ticker = tickers[0]
         self.assertIsInstance(ticker, Ticker)
@@ -31,10 +30,10 @@ class MyDataApi(unittest.TestCase):
         self.assertIsInstance(ticker.min_date, datetime.date)
 
     def test_strikes(self):
-        strikes = self._api.strikes('IBM')
+        strikes = self._api.strikes("IBM")
         for strike in strikes:
             self.assertIsInstance(strike, Strike)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     unittest.main()
