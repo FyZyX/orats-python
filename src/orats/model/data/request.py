4bc35c5bf2603691e6c2503bd933cba9f3322773
Lucas Lofaro <lucasmlofaro@gmail.com>
2022-07-14 02:37:49 -0700

    update request docstrings

--- src/orats/model/data/request.py
+++ src/orats/model/data/request.py
@@ -51,78 +51,207 @@ class _MultipleTickersHistoryTemplateRequest(_MultipleTickersTemplateRequest):
 
 
 class TickersRequest(_SingleTickerTemplateRequest):
-    pass
+    """Request duration of historical data for tickers."""
 
 
 class StrikesRequest(DataApiRequest):
-    tickers: Sequence[str] = Field(..., alias="ticker")
-    fields: Optional[Iterable[str]]
-    expiration_range: Optional[BoundedRange[int]] = Field(None, alias="dte")
-    delta_range: Optional[BoundedRange[float]] = Field(None, alias="delta")
+    """Retrieves strikes data for the given asset(s).
+
+    Specify a trade date to retrieve historical end of day values.
+    See the corresponding `Strikes`_ and `Strikes History`_ endpoints.
+    """
+
+    tickers: Sequence[str] = Field(
+        ...,
+        alias="ticker",
+        description="List of assets to retrieve.",
+    )
+    fields: Optional[Iterable[str]] = Field(
+        None,
+        description="The subset of fields to retrieve.",
+    )
+    expiration_range: Optional[BoundedRange[int]] = Field(
+        None,
+        alias="dte",
+        description="Filters results to a range of days to expiration."
+        "Specified as a ``(min, max)`` range of integers."
+        "To ignore an upper/lower bound, use `...` as a placeholder."
+        "Examples: ``(30, 45)``, ``(30, ...)``, ``(..., 45)``",
+    )
+    delta_range: Optional[BoundedRange[float]] = Field(
+        None,
+        alias="delta",
+        description="Filters results to a range of delta values."
+        "Specified as a ``(min, max)`` range of floating point numbers."
+        "To ignore an upper/lower bound, use ``...`` as a placeholder."
+        "Examples: ``(.30, .45)``, ``(.30, ...)``, ``(..., .45)``",
+    )
 
 
 class StrikesHistoryRequest(StrikesRequest):
+    """Retrieves strikes data for the given asset(s).
+
+    Specify a trade date to retrieve historical end of day values.
+    See the corresponding `Strikes`_ and `Strikes History`_ endpoints.
+    """
+
     trade_date: datetime.date = Field(..., alias="tradeDate")
 
 
 class StrikesByOptionsRequest(DataApiRequest):
-    ticker: str = Field(..., alias="ticker")
-    expiration_date: datetime.date = Field(..., alias="expirDate")
-    strike: float
+    """Retrieves strikes data by ticker, expiry, and strike.
+
+    Specify a trade date to retrieve historical end of day values.
+    See the corresponding `Strikes by Options`_ and
+    `Strikes History by Options`_ endpoints.
+    """
+
+    ticker: str = Field(
+        ...,
+        alias="ticker",
+        description="The ticker symbol of the underlying asset.",
+    )
+    expiration_date: datetime.date = Field(
+        ...,
+        alias="expirDate",
+        description="The expiration date to retrieve.",
+    )
+    strike: float = Field(
+        ...,
+        description="The strike price to retrieve.",
+    )
 
 
 class StrikesHistoryByOptionsRequest(StrikesByOptionsRequest):
-    trade_date: datetime.date = Field(..., alias="tradeDate")
+    """Retrieves strikes data by ticker, expiry, and strike.
+
+    Specify a trade date to retrieve historical end of day values.
+    See the corresponding `Strikes by Options`_ and
+    `Strikes History by Options`_ endpoints.
+    """
+
+    trade_date: datetime.date = Field(
+        ...,
+        alias="tradeDate",
+        description="The trade date to retrieve.",
+    )
 
 
 class MoniesRequest(DataApiRequest):
-    tickers: Sequence[str] = Field(..., alias="ticker")
-    fields: Optional[Iterable[str]]
+    """Retrieves end of day monthly implied history data for monies.
+
+    Specify a trade date to retrieve historical end of day values.
+    See the corresponding `Monies`_ and `Monies History`_ endpoints.
+
+    Retrieves monthly forecast history data for monies.
+
+    Specify a trade date to retrieve historical end of day values.
+    See the corresponding `Monies`_ and `Monies History`_ endpoints.
+    """
+
+    tickers: Sequence[str] = Field(
+        ...,
+        alias="ticker",
+        description="List of assets to retrieve.",
+    )
+    fields: Optional[Iterable[str]] = Field(
+        None,
+        description="The subset of fields to retrieve.",
+    )
 
 
 class MoniesHistoryRequest(MoniesRequest):
-    trade_date: datetime.date = Field(..., alias="tradeDate")
+    """Retrieves end of day monthly implied history data for monies.
+
+    Specify a trade date to retrieve historical end of day values.
+    See the corresponding `Monies`_ and `Monies History`_ endpoints.
+    """
+
+    trade_date: datetime.date = Field(
+        ...,
+        alias="tradeDate",
+        description="The trade date to retrieve.",
+    )
 
 
 class SummariesRequest(_MultipleTickersTemplateRequest):
-    pass
+    """Retrieves SMV Summary data.
+
+    Specify a trade date to retrieve historical end of day values.
+    See the corresponding `Summaries`_ and `Summaries History`_ endpoints.
+    """
 
 
 class SummariesHistoryRequest(_MultipleTickersHistoryTemplateRequest):
-    pass
+    """Retrieves SMV Summary data.
+
+    Specify a trade date to retrieve historical end of day values.
+    See the corresponding `Summaries`_ and `Summaries History`_ endpoints.
+    """
 
 
 class CoreDataRequest(_MultipleTickersTemplateRequest):
-    pass
+    """Retrieves Core history data.
+
+    Specify a trade date to retrieve historical end of day values.
+    See the corresponding `Core Data`_ and `Core Data History`_ endpoints.
+    """
 
 
 class CoreDataHistoryRequest(_MultipleTickersHistoryTemplateRequest):
-    pass
+    """Retrieves Core history data.
+
+    Specify a trade date to retrieve historical end of day values.
+    See the corresponding `Core Data`_ and `Core Data History`_ endpoints.
+    """
 
 
 class DailyPriceRequest(_MultipleTickersHistoryTemplateRequest):
-    pass
+    """Retrieves end of day daily stock price data.
+
+    See the corresponding `Daily Price`_ endpoint.
+    """
 
 
 class HistoricalVolatilityRequest(_MultipleTickersHistoryTemplateRequest):
-    pass
+    """Retrieves historical volatility data.
+
+    See the corresponding `Historical Volatility`_ endpoint.
+    """
 
 
 class DividendHistoryRequest(_SingleTickerTemplateRequest):
-    pass
+    """Retrieves dividend history data.
+
+    See the corresponding `Dividend History`_ endpoint.
+    """
 
 
 class EarningsHistoryRequest(_SingleTickerTemplateRequest):
-    pass
+    """Retrieves earnings history data.
+
+    See the corresponding `Earnings History`_ endpoint.
+    """
 
 
 class StockSplitHistoryRequest(_SingleTickerTemplateRequest):
-    pass
+    """Retrieves stock split history data.
+
+    See the corresponding `Stock Split History`_ endpoint.
+    """
 
 
 class IvRankRequest(_MultipleTickersTemplateRequest):
-    pass
+    """Retrieves IV rank data.
+
+    Specify a trade date to retrieve historical end of day values.
+    See the corresponding `IV Rank`_ and `IV Rank History`_ endpoints.
+    """
 
 
 class IvRankHistoryRequest(_MultipleTickersHistoryTemplateRequest):
-    pass
+    """Retrieves IV rank data.
+
+    Specify a trade date to retrieve historical end of day values.
+    See the corresponding `IV Rank`_ and `IV Rank History`_ endpoints.
+    """
